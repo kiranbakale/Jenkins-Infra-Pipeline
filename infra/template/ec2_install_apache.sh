499e9f17a38d5697b0289991743e25c37838a5b8
kiranbakale <kiranbakale9@gmail.com>
2024-03-05 13:33:12 +0530

    made changes to instance name

--- infra/template/ec2_install_apache.sh
+++ infra/template/ec2_install_apache.sh
@@ -6,7 +6,7 @@ yes | sudo apt install python3 python3-pip
 git clone https://github.com/kiranbakale/Python-MySQL-DB.git
 sleep 20
 # shellcheck disable=SC2164
-cd python-mysql-db-proj-1
+cd Python-MySQL-DB
 pip3 install -r requirements.txt
 echo 'Waiting for 30 seconds before running the app.py'
 setsid python3 -u app.py &
